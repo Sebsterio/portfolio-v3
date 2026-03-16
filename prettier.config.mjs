a435ef52fcddfacb77b834fde5369bd0403c5b85
Sebster <sebastian.rosloniec@gmail.com>
2026-03-16 05:47:20 +0000

    build: integrate prettier with tailwind

---
+++ prettier.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+const prettierConfig = {
+	printWidth: 140,
+	useTabs: true,
+	tabWidth: 2,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	endOfLine: "auto",
+
+	plugins: ["prettier-plugin-tailwindcss"],
+	tailwindStylesheet: "./src/styles/globals.css",
+	tailwindFunctions: ["clsx", "twMerge", "cn"],
+
+	overrides: [
+		{
+			files: ["*.json", "*.jsonc"],
+			options: {
+				printWidth: 120,
+				useTabs: false,
+			},
+		},
+	],
+};
+
+export default prettierConfig;
